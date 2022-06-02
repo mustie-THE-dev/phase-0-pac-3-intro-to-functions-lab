a1609c5149d6ef6cf2283a040bf9394b36ce8324
Simon kamau <simon.kamau@student.moringaschool.com>
2022-06-02 10:00:48 +0300

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,9 +1,12 @@
 
-describe('shout(string)', function() {
+describe('shout(string)', function () {
+
   it('receives one argument and returns it in all caps', function() {
     expect(shout('hello')).toEqual('HELLO');
+
   })
 })
+
 
 describe('whisper(string)', function() {
   it('receives one argument and returns it in all lowercase', function() {
